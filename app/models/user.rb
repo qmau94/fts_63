725f1eee183c032fbdf369309d544b57aedd5d96
qmau94 <qmau94@users.noreply.github.com>
2016-07-14 13:39:43 +0700

    refs create 69119 omniauth-facebook config devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,26 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :omniauthable, :trackable, :recoverable
+  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable
   devise :database_authenticatable, :registerable,
-    :rememberable, :validatable
+    :rememberable, :validatable, :omniauthable
   has_many :exam, dependent: :destroy
+
+  class << self
+    def from_omniauth auth
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.username = auth.info.name  
+      end
+    end
+
+    def new_with_session params, session
+      super.tap do |user|
+        if data = session["devise.facebook_data"] && 
+          session["devise.facebook_data"]["extra"]["raw_info"]
+          user.email = data["email"] if user.email.blank?
+        end
+      end
+    end
+  end
 end
--- db/migrate/20160713085229_create_users.rb
+++ db/migrate/20160713085229_create_users.rb
@@ -2,8 +2,10 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.boolean :is_admin?
-
+      t.boolean :is_admin, default: false
+      t.string :provider
+      t.string :uid
+      
       t.timestamps null: false
     end
   end
